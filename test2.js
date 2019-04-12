5d38fefa41460d486ccdcc132b2b2528044229c3
irage <hiteshbhatia3559@gmail.com>
2019-04-12 14:19:48 +0530

    Progress

--- test2.js
+++ test2.js
@@ -7,7 +7,7 @@ let request = require('request');
 let fs = require('fs');
 
 let query1 = 'http://172.16.100.227:8086/query?db=trade&q=SELECT mean("buyValuePos") AS "mean_buyValuePos", mean("sellValuePos") AS "mean_sellValuePos" FROM "trade"."autogen"."UserTradePositionInfo" WHERE time > now() - 12h and "instrument_type"=\'"XX"\' GROUP BY time(1d), expiry, underlying';
-let query2 = 'http://172.16.100.227:8086/query?db=trade&q=SELECT mean("buyValuePos") AS "mean_buyValuePos", mean("sellValuePos") AS "mean_sellValuePos" FROM "trade"."autogen"."UserTradePositionInfo" WHERE time > now() - 12h and "instrument_type"=\'"XX"\' GROUP BY time(1h), expiry, underlying';
+// let query2 = 'http://172.16.100.227:8086/query?db=trade&q=SELECT mean("buyValuePos") AS "mean_buyValuePos", mean("sellValuePos") AS "mean_sellValuePos" FROM "trade"."autogen"."UserTradePositionInfo" WHERE time > now() - 12h and "instrument_type"=\'"XX"\' GROUP BY time(1h), expiry, underlying';
 
 
 request(query1,
@@ -20,9 +20,30 @@ request(query1,
         fs.writeFileSync('./data/saved.json', data);
     });
 
+
 http.createServer(function (req, res) {
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.write(JSON.stringify(value))
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    arraytowrite = [];
+    value.forEach(function (item) {
+        // res.write(JSON.stringify({item["columns"][0]:item["values"][1][0]}));
+        value_to_write = {};
+        value_to_write["time"] = item["values"][1][0];
+        value_to_write["underlying"] = item["tags"]["underlying"];
+        value_to_write["NetTradedValue"] = item["values"][1][1] + item["values"][1][2];
+        arraytowrite.push(value_to_write)
+    });
+    res.write(JSON.stringify(arraytowrite));
+    var fs = require('fs');
+    fs.writeFile('data.json',JSON.stringify(arraytowrite),'utf8',function(err, result) {
+     if(err) console.log('error', err);
+   });
+
+    // res.write("]");
 }).listen(port, ip);
+
 
 console.log('Server running at http://' + ip + ':' + port);
